fe1de2a5a1c4d7d5d93e55df09fbbe0711d74ee8
aokigahara <moun-lord@yandex.ru>
2022-08-22 22:09:22 +0300

    Add methods for chanhe currency

--- PracticeWithTypes/Program.cs
+++ PracticeWithTypes/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Models;
+
+namespace PracticeWithTypes
+{
+    class Program
+    {
+
+        public void Main(string[] args)
+        {
+
+        }
+
+
+
+        // Неправильный метод обновления служащего
+        public void wrongUpatecontract(Employee employee)
+        {
+            employee.contract = $"Сотрудник {employee.name}, {employee.lastName} был принят на работу на следующие 3 года.";
+        }
+
+
+        // Правильный метод
+        public string updateContract(Employee employee)
+        {
+            return $"Сотрудник {employee.name}, {employee.lastName} был принят на работу на следующие 3 года.";
+        }
+
+
+    }
+}
