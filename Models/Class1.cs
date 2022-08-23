0ea981f56bfd44dce715b2b8a701407b97522d1c
aokigahara <moun-lord@yandex.ru>
2022-08-23 09:47:06 +0300

    Bug fixes in Practice with Types and Models

--- Models/Class1.cs
+++ Models/Class1.cs
@@ -1,7 +1,29 @@
 ﻿namespace Models
 {
-    public class Class1
+    public class Person
     {
+        public string name { get; set; }
+        public string lastName { get; set; }
+        public int passport_id { get; set; }
+        public DateTime birthday { get; set; }
+
+    }
+
+    public class Employee : Person
+    {
+        public string contract { get; set; }
+    }
+
+    public class Client : Person
+    {
+
+    }
+
+
+    public struct Currency
+    {
+        public string name { get; set; }
+        public int id { get; set; }
 
     }
 }
--- PracticeWithTypes/Program.cs
+++ PracticeWithTypes/Program.cs
@@ -2,29 +2,59 @@
 
 namespace PracticeWithTypes
 {
-    class Program
+    internal class Program
     {
 
-        public void Main(string[] args)
+        static void Main(string[] args)
         {
+            Employee employee = new Employee { name = "Григорий", lastName = "Пихта"};
+            employee.contract = updateContract(employee);
+            //wrongUpatecontract(employee);
+            Console.WriteLine(employee.contract);
+            
 
+
+            Currency currency = new Currency { name = "USD", id = 1};
+            //wrondChangeCurrency(currency);
+            currency = changeCurrency(currency);
+            Console.WriteLine(currency.name + " " + currency.id);
+            Console.ReadLine();
         }
 
 
 
         // Неправильный метод обновления служащего
-        public void wrongUpatecontract(Employee employee)
+        public static void wrongUpatecontract(Employee employee)
         {
-            employee.contract = $"Сотрудник {employee.name}, {employee.lastName} был принят на работу на следующие 3 года.";
+            employee.contract = $"Сотрудник {employee.name} {employee.lastName} был принят на работу на следующие 3 года.";
         }
 
-
-        // Правильный метод
-        public string updateContract(Employee employee)
+        // Правильный метод обновления служащего
+        public static string updateContract(Employee employee)
         {
             return $"Сотрудник {employee.name}, {employee.lastName} был принят на работу на следующие 3 года.";
         }
 
 
+
+
+        // Нерабочий метод
+        public static void wrondChangeCurrency(Currency currency)
+        {
+            currency.name = "Рубль";
+            currency.id = 810;
+        }
+
+
+        // Рабочий метод
+        public static Currency changeCurrency(Currency currency)
+        {
+            currency.name = "Рубль";
+            currency.id = 810;
+
+            return currency;
+        }
     }
+
+    
 }
