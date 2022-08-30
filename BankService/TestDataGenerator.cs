f53450499494ab165bf59fadc00e50fb8bd9afe1
aokigahara59 <doble-you@yandex.ru>
2022-08-30 17:17:36 +0300

    Add name generating with Bogus

--- BankService/TestDataGenerator.cs
+++ BankService/TestDataGenerator.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Models;
-
+using Bogus;
 
 namespace Services
 {
@@ -15,11 +15,12 @@ namespace Services
         {
             var clients = new List<Client>();
             Random random = new Random();
+            Bogus.Faker faker = new Bogus.Faker();
             for (int i = 0; i < 1000; i++)
             {
                 clients.Add(new Client
                 {
-                    Name = "Name" + i,
+                    Name = faker.Name.FirstName(),
                     Birthday = new DateTime(random.Next(20, 100)),
                     PhoneNumber = i
                 });
@@ -31,11 +32,12 @@ namespace Services
         {
             var clients = new Dictionary<int, Client>();
             Random random = new Random();
-            for(int i = 0; i < 1000; i++)
+            Bogus.Faker faker = new Bogus.Faker();
+            for (int i = 0; i < 1000; i++)
             {
                 Client client = new Client
                 {
-                    Name = "Name" + i,
+                    Name = faker.Name.FirstName(),
                     Birthday = new DateTime(random.Next(20, 100)),
                     PhoneNumber = i
                 };
@@ -48,11 +50,12 @@ namespace Services
         public List<Employee> GenerateTestEmployeeList()
         {
             var employees = new List<Employee>();
-            for(int i = 0; i < 1000; i++)
+            Bogus.Faker faker = new Bogus.Faker();
+            for (int i = 0; i < 1000; i++)
             {
                 employees.Add(new Employee
                 {
-                    Name = "Name" + i,
+                    Name = faker.Name.FirstName(),
                     Salary = 10000-i,
                 });
             }
