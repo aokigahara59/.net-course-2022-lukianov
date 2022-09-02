dc08220ffd5f1b8d6dd5c357e12c5d0aa5dc7eaf
aokigahara59 <doble-you@yandex.ru>
2022-09-02 13:01:58 +0300

    Add GetHashCodeNecessityPositivTest for client
    Override Equals and GetHashCode for Client

--- Models/Client.cs
+++ Models/Client.cs
@@ -9,5 +9,25 @@ namespace Models
     public class Client : Person
     {
         public string PhoneNumber { get; set; }
+
+        public override bool Equals(object? obj)
+        {
+            if (obj == null) return false;
+            if (!(obj is Client)) return false;
+
+            Client client = (Client) obj;
+            return Name == client.Name 
+                && LastName == client.LastName
+                && PassportId == client.PassportId
+                && PhoneNumber == client.PhoneNumber;
+        }
+
+        public override int GetHashCode()
+        {
+            return Name.GetHashCode()
+                + LastName.GetHashCode()
+                + PassportId.GetHashCode()
+                + PhoneNumber.GetHashCode();
+        }
     }
 }
---
+++ Service.Tests/EquivalenceTests.cs
@@ -0,0 +1,36 @@
+﻿using Xunit;
+using Models;
+using Services;
+
+namespace Service.Tests
+{
+    public class EquivalenceTests
+    {
+
+        [Fact]
+        public void GetHashCodeNecessityPositivTest()
+        {
+            // Arrange
+            TestDataGenerator dataGenerator = new TestDataGenerator();
+            var accountDictionary = dataGenerator.GenerateClientAccountDictionary();
+            Client lastClient = accountDictionary.Keys.Last();
+            Client newClient = new Client
+            {
+                Name = lastClient.Name,
+                LastName = lastClient.LastName,
+                Birthday = lastClient.Birthday,
+                PhoneNumber = lastClient.PhoneNumber,
+                PassportId = lastClient.PassportId
+            };
+
+            // Act
+            bool indicator = false;
+            Account account = accountDictionary[newClient];           
+            if (account != null) indicator = true;
+
+
+            // Assert
+            Assert.True(indicator);
+        }
+    }
+}
