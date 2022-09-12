2360ca9d1a5f5b88028a0165054e6327e736e5a0
aokigahara59 <doble-you@yandex.ru>
2022-09-12 23:22:52 +0300

    GenericType-1 feature: add storage and client storage interface

---
+++ Services/Storages/IClientStorage.cs
@@ -0,0 +1,12 @@
+﻿using Models;
+
+namespace Services.Storages
+{
+    public interface IClientStorage : IStorage<Client>
+    {
+        Dictionary<Client, List<Account>> Data { get; }
+        void AddAccount(Client client, Account account);
+        void RemoveAccount(Client client, Account account);
+        void UpdateAccount(Client client, Account account);
+    }
+}
---
+++ Services/Storages/IStorage.cs
@@ -0,0 +1,9 @@
+﻿namespace Services.Storages
+{
+    public interface IStorage<T>
+    {
+        void Add(T item);
+        void Delete(T item);
+        void Update(T item);
+    }
+}
