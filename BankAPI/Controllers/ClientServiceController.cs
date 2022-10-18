b86a83bdecee7e99213505308eaa1df2b3468c50
aokigahara59 <doble-you@yandex.ru>
2022-10-19 00:46:03 +0300

    Controllers-1 feat: add controllers for client and employee services

---
+++ BankAPI/Controllers/ClientServiceController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Models;
+using Services;
+using Services.Filters;
+
+namespace BankAPI.Controllers
+{
+    [ApiController]
+    [Route("ClientServiceController")]
+    public class ClientServiceController
+    {
+        private ClientService _clientService;
+
+        public ClientServiceController()
+        {
+            _clientService = new ClientService();
+        }
+
+
+        [HttpPost(Name = "AddClient")]
+        public async void AddClient(Client client)
+        {
+            await _clientService.AddClientAsync(client);
+        }
+
+
+        [HttpGet(Name = "GetClient")]
+        public Client GetClient(Guid id)
+        {
+            return _clientService.GetClient(id);
+        }
+
+
+        [HttpPut(Name = "UpdateClient")]
+        public async void UpdateClient(Guid id, Client client)
+        {
+            await _clientService.UpdateClientAsync(id, client);
+        }
+
+
+        [HttpDelete(Name = "DeleteClient")]
+        public async void DeleteClient(Guid id)
+        {
+            await _clientService.DeleteClientAsync(id);
+        }
+    }
+}
---
+++ BankAPI/Controllers/EmployeeServiceController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Models;
+using Services;
+
+namespace BankAPI.Controllers
+{
+    [ApiController]
+    [Route("EmployeeServiceController")]
+    public class EmployeeServiceController
+    {
+        private EmployeeService _employeeService;
+
+        public EmployeeServiceController()
+        {
+            _employeeService = new EmployeeService();
+        }
+
+
+        [HttpPost(Name = "AddEmployee")]
+        public async void AddEmployee(Employee employee)
+        {
+            await _employeeService.AddEmployeeAsync(employee);
+        }
+
+
+        [HttpGet(Name = "GetEmployee")]
+        public Employee GetEmployee(Guid id)
+        {
+            return _employeeService.GetEmployee(id);
+        }
+
+
+        [HttpPut(Name = "UpdateEmployee")]
+        public async void UpdateEmployee(Guid id, Employee employee)
+        {
+            await _employeeService.UpdateEmployeeAsync(id, employee);
+        }
+
+
+        [HttpDelete(Name = "DeleteEmployee")]
+        public async void DeleteEmployee(Guid id)
+        {
+            await _employeeService.DeleteEmployeeAsync(id);
+        }
+    }
+}
