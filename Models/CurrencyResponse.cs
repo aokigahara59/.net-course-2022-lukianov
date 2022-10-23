182a6aaffad1ec31499477fa5706f3f682422196
aokigahara59 <doble-you@yandex.ru>
2022-10-23 12:42:02 +0300

    HttpClient-1 feat: add currency exchange methods through http request

---
+++ Models/CurrencyResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace Models
+{
+    public class CurrencyResponse
+    {
+        public int Error { get; set; }
+        public string Error_Message { get; set; }
+        public float Amount { get; set; }
+    }
+}
---
+++ Services.Tests/CurrencyServiceTests.cs
@@ -0,0 +1,28 @@
+﻿using Models;
+using Xunit;
+
+namespace Services.Tests
+{
+    public class CurrencyServiceTests
+    {
+
+        [Fact]
+        public async void CurrencyExchangePositivTest()
+        {
+            // Arrange
+            CurrencyService currencyService = new CurrencyService();
+
+            Currency fromCurrency = new Currency { Name = "USD" };
+            Currency toCurrency = new Currency { Name = "RUB" };
+
+            // Act
+            currencyService.Authorize("fn4BrxqVaPu2YkrswXvad2Cug5Pdan");
+
+            var response = await currencyService.ExchangeMoneyAsync(fromCurrency, toCurrency, 500);
+
+            // Assert
+            Assert.NotEqual(0, response.Amount);
+        }
+
+    }
+}
